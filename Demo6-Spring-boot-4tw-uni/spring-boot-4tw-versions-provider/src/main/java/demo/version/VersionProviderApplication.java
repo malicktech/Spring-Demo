a53cf320132a950303cbe221d6d3771e0a587be0
Malick <diop-malick@hotmail.fr>
2016-07-12 09:08:52 +0200

    [demo6 - spring-boot-4tw-web] update pom for executable jar and correct link in app.js

--- Demo6-Spring-boot-4tw-uni/spring-boot-4tw-versions-provider/src/main/java/demo/version/VersionProviderApplication.java
+++ Demo6-Spring-boot-4tw-uni/spring-boot-4tw-versions-provider/src/main/java/demo/version/VersionProviderApplication.java
@@ -21,34 +21,34 @@ import org.springframework.web.client.RestTemplate;
 @EnableConfigurationProperties(SpringBootVersionProperties.class)
 public class VersionProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VersionProviderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VersionProviderApplication.class, args);
+    }
 
-	@Bean
-	public JCacheManagerCustomizer cacheManagerCustomizer() {
-		return cm -> {
-			cm.createCache("boot-versions", new MutableConfiguration<>()
-					.setExpiryPolicyFactory(CreatedExpiryPolicy
-							.factoryOf(Duration.ONE_HOUR))
-					.setStoreByValue(false)
-					.setStatisticsEnabled(true));
-		};
-	}
+    @Bean
+    public JCacheManagerCustomizer cacheManagerCustomizer() {
+        return cm -> {
+            cm.createCache("boot-versions", new MutableConfiguration<>()
+                    .setExpiryPolicyFactory(CreatedExpiryPolicy
+                            .factoryOf(Duration.ONE_HOUR))
+                    .setStoreByValue(false)
+                    .setStatisticsEnabled(true));
+        };
+    }
 
-	@Bean
-	public HealthIndicator mavenRepositoriesHealthIndicator(SpringBootVersionService springBootVersionService) {
-		return new AbstractHealthIndicator() {
-			@Override
-			protected void doHealthCheck(Health.Builder builder) throws Exception {
-				RestTemplate restTemplate = new RestTemplate();
-				for (String url : springBootVersionService.getRepositoryUrls()) {
-					ResponseEntity<String> entity = restTemplate
-							.getForEntity(url, String.class);
-					builder.up().withDetail(url, entity.getStatusCode());
-				}
-			}
-		};
-	}
+    @Bean
+    public HealthIndicator mavenRepositoriesHealthIndicator(SpringBootVersionService springBootVersionService) {
+        return new AbstractHealthIndicator() {
+            @Override
+            protected void doHealthCheck(Health.Builder builder) throws Exception {
+                RestTemplate restTemplate = new RestTemplate();
+                for (String url : springBootVersionService.getRepositoryUrls()) {
+                    ResponseEntity<String> entity = restTemplate
+                            .getForEntity(url, String.class);
+                    builder.up().withDetail(url, entity.getStatusCode());
+                }
+            }
+        };
+    }
 
 }
--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Set of Sample app used for experimental test of  the features of the Spring Fram
 ## demo 6 | Spring-boot-4tw-uni
 
 Maîtrisez l’auto-configuration Spring Boot par Stéphane Nicoll (FR)  
-    [](https://www.youtube.com/watch?v=jwATN8mou1E)
+    [](https://www.youtube.com/watch?v=sR8PyhJa-Zw)
